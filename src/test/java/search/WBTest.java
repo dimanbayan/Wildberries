778034d0a49aebfe08db1602002e47843aa50163
dmitry mumolin <dmitrbayan5@yandex.ru>
2023-08-11 23:55:38 +0300

    first commit

---
+++ src/test/java/search/WBTest.java
@@ -0,0 +1,20 @@
+package search;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class WBTest {
+    @Test
+    void successLoginTest(){
+        Configuration.holdBrowserOpen = true;
+
+        open("https://www.wildberries.ru/");
+        sleep(5000);
+        $("[id=\"searchInput\"]").setValue("пружинки для кота");
+        $(".search-catalog__btn--search").click();
+        $(".product-card__name").shouldHave(text("Игрушка"));
+    }
+}
